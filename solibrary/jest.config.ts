49637195b8f95a7a925bae442298838434341783
8 bit <8bit@ordlibrary.com>
2024-08-01 14:27:52 -0400

    feat(nx): Added Nx Cloud token to your nx.json
              To connect your workspace to Nx Cloud, push your repository
              to your git hosting provider and go to the following URL:
    
              https://cloud.nx.app/connect/GvBJ91Y2Iy

---
+++ solibrary/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'solibrary',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/solibrary',
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.[jt]s?(x)', '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)'],
+}
